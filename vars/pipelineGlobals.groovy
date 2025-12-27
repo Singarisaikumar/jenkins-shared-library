8660b14dbcbd27956c80e746732661161eccffc9
saikumar <wwwsaikumar150@gmail.com>
2025-12-27 13:57:50 +0530

    jenkins

--- vars/pipelineGlobals.groovy
+++ vars/pipelineGlobals.groovy
@@ -7,3 +7,20 @@ static final String nexusURL(){
 static final String account_id(){
     return "192990275288"
 }
+
+def getAccountID(String environment){
+    switch(environment) { 
+        case 'dev': 
+            return "192990275288"
+        case 'qa':
+            return "192990275288"
+        case 'uat':
+            return "192990275288"
+        case 'pre-prod':
+            return "192990275288"
+        case 'prod':
+            return "192990275288"
+        default:
+            return "nothing"
+    } 
+}
